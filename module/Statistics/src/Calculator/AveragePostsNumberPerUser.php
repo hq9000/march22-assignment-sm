5b199d007dbd5003ffbae27a1c8348f3fbc90b3f
Sergey Grechin <grechin.sergey@gmail.com>
2022-03-13 09:56:57 +0300

    added a calculator, modified the factory

---
+++ module/Statistics/src/Calculator/AveragePostsNumberPerUser.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace Statistics\Calculator;
+
+
+use SocialPost\Dto\SocialPostTo;
+use Statistics\Dto\StatisticsTo;
+
+class AveragePostsNumberPerUser extends AbstractCalculator
+{
+
+    protected const UNITS = 'posts';
+
+    private int $postCount = 0;
+
+    /**
+     * @var array<string, bool>
+     *
+     * contains a map of unique user_id values to true
+     */
+    private array $usersMap = [];
+
+    protected function doAccumulate(SocialPostTo $postTo): void
+    {
+        $this->postCount++;
+        $this->usersMap[$postTo->getAuthorId()] = true;
+    }
+
+    protected function doCalculate(): StatisticsTo
+    {
+        $uniqueUsersCount = count($this->usersMap);
+
+        $value = $this->postCount > 0
+            ? $this->postCount / $uniqueUsersCount
+            : 0;
+
+        return (new StatisticsTo())->setValue(round($value,2));
+    }
+}
